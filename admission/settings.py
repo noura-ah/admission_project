d72b165c5337b320bf0fb08d4f9f6cbf6500576d
shahad alharbi <shahadalharbi.m@gmail.com>
2022-06-27 23:25:14 +0300

    edit login and register page +logo

--- admission/settings.py
+++ admission/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
